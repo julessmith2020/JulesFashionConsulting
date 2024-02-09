01e66ccc7eb74be0fa4e94ca2753446e9cf51c00
Jules Smith <jusmith@byu.edu>
2024-02-08 20:31:21 -0800

    Add project files.

---
+++ JulesFashionConsulting/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#calculateTotal').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var hourlyRate = parseFloat($('#hourlyRate').val());
+
+        if (isNaN(hours) || hours <= 0) {
+            $('.error').text('Please enter a valid positive number of hours.');
+            return;
+        }
+
+        $('.error').text('');
+        var total = hours * hourlyRate;
+        $('#total').text('$' + total.toFixed(2));
+    });
+});
